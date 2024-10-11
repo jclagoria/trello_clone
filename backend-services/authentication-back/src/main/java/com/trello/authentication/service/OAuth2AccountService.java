0233479b0c09fc8d118633c367aa8aec66b91466
Juan Lagoria <lagoria79@gmail.com>
2024-10-11 00:06:56 -0300

    delete files not necesaries

--- backend-services/authentication-back/src/main/java/com/trello/authentication/service/OAuth2AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.trello.authentication.service;
-
-import com.trello.authentication.model.User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import reactor.core.publisher.Mono;
-
-public interface OAuth2AccountService {
-    Mono<User> processOAuth2User(OAuth2User oAuth2User);
-}
