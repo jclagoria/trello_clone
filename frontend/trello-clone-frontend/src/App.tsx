947d501c045c3e7bc2be3b4cf563cdd675b1613d
JuanKa Lagoria <lagoria79@gmail.com>
2024-11-01 15:03:27 -0300

    add changes

--- frontend/trello-clone-frontend/src/App.tsx
+++ frontend/trello-clone-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Header } from "./components/Header"
 import { HomePage } from "./components/HomePage"
 import { SignUpPage } from "./components/SignUpPage"
+import {Footer} from "./components/Footer.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" Component={HomePage} />
           <Route path="/signup" Component={SignUpPage}/>
         </Routes>
+        <Footer />
       </div>
     </Router>
   )
